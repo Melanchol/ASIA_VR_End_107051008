a12f9485e8eb52f56f4261ffcc004b718bd9131a
Melanchol <71679869+Melanchol@users.noreply.github.com>
2021-01-12 00:34:32 +0800

    套不了GameManager
    我真的不知道為什麼

---
+++ VR_End_107051008/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager: MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("VRsence");
+    }
+
+    // Update is called once per frame
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
