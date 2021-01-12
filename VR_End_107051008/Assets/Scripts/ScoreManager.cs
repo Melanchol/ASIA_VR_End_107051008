d4a01e2a2d0fee27081a6f795d4d1342b8e5c084
Melanchol <71679869+Melanchol@users.noreply.github.com>
2021-01-12 14:46:04 +0800

    發布EXE檔案

--- VR_End_107051008/Assets/Scripts/ScoreManager.cs
+++ VR_End_107051008/Assets/Scripts/ScoreManager.cs
@@ -1,25 +1,45 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;   // 引用 介面 API
+using UnityEngine.UI;  // 引用 介面 API
+using UnityEngine.UIElements;
 
 public class ScoreManager : MonoBehaviour
 {
     [Header("分數介面")]
     public Text textScore;
     [Header("分數")]
-    public int score;
-    [Header("投進的分數")]
-    public int scoreIn = 2;
-    [Header("進球音效")]
-    public AudioClip soundIn;
+    public int score=0;
+    [Header("加的分數")]
+    public int scoreIn = 1;
+    [Header("時間字樣")]
+    public Text T_timeText;
+    [Header("開始開關")]
+    public bool start=false;
 
-    private AudioSource aud;
-
-    private void Awake()
+    public float T_time = 30;
+    
+    public void StartGame()
     {
-        // 音效來源 = 取得元件<音效來源>()
-        aud = GetComponent<AudioSource>();
+        T_time = 30;
+        score = 0;
+        start = true;
+        
     }
+    
+    void Update()
+    {
+        if (start == true)
+        {
 
+
+            T_time -= Time.deltaTime;
+            T_timeText.text = "Time:" + T_time.ToString("0.0");
+            print(T_timeText);
+        }
+        if (T_time <= 0) { start = false; }
+    }
+    bool t, c, k, f;
+
+    
     // OTE 條件：
     // 兩個碰撞物件 其中一個 必須勾選 IsTrigger
     // 要偵測此腳本子物件是否產生碰撞
@@ -27,26 +47,53 @@ public class ScoreManager : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         // 如果 碰撞物件的標籤 為 籃球 就加分 並且 籃球 的 高度 > 2.5
-        if (other.tag == "籃球" && other.transform.position.y > 2.5f)
+        
+
+        if (other.tag == "Tray")
         {
+            t = true;
+        }
+        if(other.tag=="coke")
+        {
+            c = true; 
+        }
+        else if (other.tag == "ketchup") { k = true; }
+        else if (other.tag == "fries") { f = true; }
+
+        if (t == true)
+        {
+            if (c == true) {
+                if (k == true) { scoreIn = 2; }
+                else if (f == true) { scoreIn = 2; }
+                else { scoreIn = 1; } 
+            }
+            if (k == true) {
+                if (c == true) { scoreIn = 2; }
+                else if (f == true) { scoreIn = 2; }
+                else { scoreIn = 1; }
+            }
+            if (f == true) {
+                if (k == true) { scoreIn = 2; }
+                else if (c == true) { scoreIn = 2; }
+                else { scoreIn = 1; }
+           }
+            if (c == true && k == true && f == true) { scoreIn = 6; }
             AddScore();
         }
         // 如果 碰撞的根物件名稱為 Player
         if (other.transform.root.name == "Player")
         {
-            // 玩家進入三分區域，將投進的分數改為三分
-            scoreIn = 3;
+            
         }
     }
 
     // 當碰撞物件離開碰撞範圍時執行一次
     private void OnTriggerExit(Collider other)
     {
-        if (other.transform.root.name == "Player")
-        {
-            // 將投進的分數改為兩分
-            scoreIn = 2;
-        }
+        t = false;
+        c = false;
+        k = false;
+        f = false;
     }
 
     /// <summary>
@@ -55,7 +102,7 @@ public class ScoreManager : MonoBehaviour
     private void AddScore()
     {
         score += scoreIn;                                   // 分數遞增 投進的分數
-        textScore.text = "分數：" + score;                   // 更新介面
-        aud.PlayOneShot(soundIn, Random.Range(1f, 2f));     // 音效來源.播放一次(音效片段，音量)
+        textScore.text = "Score：" + score;                   // 更新介面
+          
     }
 }
